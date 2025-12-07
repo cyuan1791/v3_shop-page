d4afcffbaf71933ee8f4a27f44015a2a04dac47e
Mark Yuan <mark@webasone.com>
2025-12-07 06:29:38 +0000

    update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import Search from './Search.vue'
-import { useCartStore } from '@/store/cart'
+import { computed } from "vue";
+import Search from "./Search.vue";
+import { useCartStore } from "@/store/cart";
 
-const cartStore = useCartStore()
+const cartStore = useCartStore();
 
-const count = computed(() => cartStore.count)
+const count = computed(() => cartStore.count);
 </script>
 
 <template>
-  <div class="d-flex justify-content-center ">
-    <div class="btn-group m-4 p-3 w-75 bg-success-subtle" role="group" aria-label="Basic mixed styles example">
+  <div class="d-flex justify-content-center">
+    <div
+      class="btn-group m-4 p-3 w-75 bg-success-subtle"
+      role="group"
+      aria-label="Basic mixed styles example"
+    >
       <router-link class="btn btn-ghost rounded-btn" to="/">
         <span class="btn btn-outline-dark"> Shop </span>
       </router-link>
-      <router-link class="btn  rounded-btn" to="/cart">
-        <span class="btn btn-outline-danger"> Cart &nbsp;&nbsp;
+      <router-link class="btn rounded-btn" to="/cart">
+        <span class="btn btn-outline-danger">
+          Cart &nbsp;&nbsp;
           <div class="badge text-bg-secondary" v-text="count" />
         </span>
       </router-link>
-      <Search />
     </div>
+  </div>
+  <div class="d-flex justify-content-center">
+    <Search />
   </div>
 </template>
--- src/composables/usePersistCart.ts
+++ src/composables/usePersistCart.ts
@@ -1,16 +1,17 @@
-import { onUnmounted } from 'vue'
-import { useCartStore } from '@/store/cart'
+import { onUnmounted } from "vue";
+import { useCartStore } from "@/store/cart";
+import { asonePath } from "@/shared/utils";
 
-export const CART_STORAGE = 'CART_STORAGE'
+export const CART_STORAGE = "CART_STORAGE" + asonePath;
 
 export function usePersistCart() {
-  const cartStore = useCartStore()
+  const cartStore = useCartStore();
 
   const unsub = cartStore.$subscribe(() => {
-    localStorage.setItem(CART_STORAGE, JSON.stringify(cartStore.contents))
-  })
+    localStorage.setItem(CART_STORAGE, JSON.stringify(cartStore.contents));
+  });
 
   onUnmounted(() => {
-    unsub()
-  })
+    unsub();
+  });
 }
